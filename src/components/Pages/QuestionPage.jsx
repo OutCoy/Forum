10a112f53f09063c18626d65d5e55cd764460898
OutCoy <cizikasg@gmail.com>
2023-05-18 15:39:24 +0300

    Added answering functionality

--- src/components/Pages/QuestionPage.jsx
+++ src/components/Pages/QuestionPage.jsx
@@ -5,6 +5,9 @@ import QuestionsContext from "../../contexts/QuestionsContext";
 import UsersContext from "../../contexts/UsersContext";
 import AnswersContext from "../../contexts/AnswersContext";
 import Answer from "../Molecules/Answer";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { v4 as uuid } from "uuid";
 
 const StyledQuestionPage = styled.main`
   background-color: #0b0e0f;
@@ -12,39 +15,97 @@ const StyledQuestionPage = styled.main`
   color: #fff;
 `;
 
+const StyledAnswerQuestion = styled.div``;
+
 const QuestionPage = () => {
-
   const navigate = useNavigate();
-  const {answers} = useContext(AnswersContext);
-  const {logedUser} = useContext(UsersContext);
+  const { answers, setAnswers, AnswersActionsType } =
+    useContext(AnswersContext);
+  const { logedUser } = useContext(UsersContext);
   const { id } = useParams();
-  const { questions, setQuestions, QuestionsActionsType } = useContext(QuestionsContext);
-  const data = questions.find(question => question.id === id);
-  const filteredAnswers = answers.filter(answer => answer.questionId === id);
-  console.log(filteredAnswers);
+  const { questions, setQuestions, QuestionsActionsType } =
+    useContext(QuestionsContext);
+  const data = questions.find((question) => question.id === id);
+  const filteredAnswers = answers.filter((answer) => answer.questionId === id);
 
-  const deleteQuestion = () =>{
+  const formik = useFormik({
+    initialValues: {
+      answer: "",
+    },
+    validationSchema: Yup.object({
+      answer: Yup.string().required(
+        "You need to type your answer bedore publishing it."
+      ),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      const newAnswer = {
+        id: uuid(),
+        questionId: id,
+        userId: logedUser.id,
+        answer: values.answer,
+        rating: [],
+        isEdited: false,
+      };
+      console.log(newAnswer);
+      setAnswers({
+        type: AnswersActionsType.add,
+        data: newAnswer,
+      });
+      formik.resetForm();
+    },
+  });
+
+  const deleteQuestion = () => {
     setQuestions({
       id: id,
       type: QuestionsActionsType.del,
     });
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
     <StyledQuestionPage>
       {
-        data.userId === logedUser?.id && <><Link to={`/editQuestion/${id}`}>Edit</Link><button onClick={() => deleteQuestion()}>Delete</button></>
-      }
+        data ? <>
+        {data.userId === logedUser?.id && (
+        <>
+          <Link to={`/editQuestion/${id}`}>Edit</Link>
+          <button onClick={() => deleteQuestion()}>Delete</button>
+        </>
+      )}
       <h1>{data.title}</h1>
       <p>{data.question}</p>
       <div>
-        {
-          filteredAnswers.map(answer => <Answer data={answer} key={answer.id}/>)
-        }
+        {filteredAnswers.map((answer) => (
+          <Answer data={answer} key={answer.id} />
+        ))}
       </div>
+      {logedUser && (
+        <StyledAnswerQuestion>
+          <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="answer">Answer</label>
+            <textarea
+              name="answer"
+              id="answer"
+              placeholder="Type your answer here..."
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.answer}
+            ></textarea>
+            <input type="submit" value="Add answer" />
+            {formik.touched.answer && formik.errors.answer && (
+              <p>{formik.errors.answer}</p>
+            )}
+          </form>
+        </StyledAnswerQuestion>
+      )}
+        </> : <>
+        <h1>Data is being loaded</h1>
+        <p>Please wait...</p></>
+      }
     </StyledQuestionPage>
   );
-}
- 
+};
+
 export default QuestionPage;
--- src/contexts/AnswersContext.jsx
+++ src/contexts/AnswersContext.jsx
@@ -3,13 +3,23 @@ import { createContext, useEffect, useReducer } from "react";
   const AnswersContext = createContext(null);
 
   const AnswersActionsType = {
-    get: 'get_all_answers'
+    get: 'get_all_answers',
+    add: 'add_new_answer'
   }
 
   const reducer = (state, action) => {
     switch(action.type){
       case AnswersActionsType.get:
         return action.data;
+      case AnswersActionsType.add:
+        fetch('http://localhost:8080/answers', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(action.data)
+        });
+        return [...state, action.data];
       default:
         return state;
     }
