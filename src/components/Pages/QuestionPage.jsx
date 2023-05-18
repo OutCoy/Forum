0a29b9afb909ff2c932afd9902c3d2824ac748e3
OutCoy <cizikasg@gmail.com>
2023-05-18 09:56:26 +0300

    Added feature that only who asked question could edit it

--- src/components/Pages/QuestionPage.jsx
+++ src/components/Pages/QuestionPage.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import QuestionsContext from "../../contexts/QuestionsContext";
+import UsersContext from "../../contexts/UsersContext";
 
 const StyledQuestionPage = styled.main`
   background-color: #0b0e0f;
@@ -11,13 +12,16 @@ const StyledQuestionPage = styled.main`
 
 const QuestionPage = () => {
 
+  const {logedUser} = useContext(UsersContext);
   const { id } = useParams();
   const { questions } = useContext(QuestionsContext);
   const data = questions.find(question => question.id === id);
 
   return (
     <StyledQuestionPage>
-      <Link to={`/editQuestion/${id}`}>Edit</Link>
+      {
+        data.userId === logedUser.id && <Link to={`/editQuestion/${id}`}>Edit</Link>
+      }
       <h1>{data.title}</h1>
     </StyledQuestionPage>
   );
