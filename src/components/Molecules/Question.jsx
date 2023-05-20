f8c1302a88afb573234b14dc000ce358645b4be9
OutCoy <cizikasg@gmail.com>
2023-05-20 10:25:25 +0300

    Added questions home page styling

--- src/components/Molecules/Question.jsx
+++ src/components/Molecules/Question.jsx
@@ -1,7 +1,35 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledQuestion = styled.div``;
+const StyledQuestion = styled.div`
+  width: 100%;
+  background-color: #1a1c2c;
+  box-sizing: border-box;
+  border-radius: 5px;
+  height: 105px;
+  overflow: hidden;
+  >a{
+    text-decoration: none;
+    color: #fff;
+    width: 100%;
+    box-sizing: border-box;
+    display: inline-block;
+    padding: 0 20px;
+    height: 100%;
+    >p{
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    >h2{
+      margin: 10px 0;
+      width: 100%;
+    }
+  }
+  >a:hover{
+    background-color: #2385fc45;
+  }
+`;
 
 const Question = ({ data }) => {
   return (
