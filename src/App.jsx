9877f1405498699db5aa70903c35c02ac2757d68
OutCoy <cizikasg@gmail.com>
2023-05-22 10:42:32 +0300

    Added redirects if not logged user tries to edit or ask new question

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Pages/Home';
 import QuestionPage from './components/Pages/QuestionPage';
@@ -7,14 +7,19 @@ import AskQuestion from './components/Pages/AskQuestion';
 import EditQuestion from './components/Pages/EditQuestion';
 import Header from './components/Organisms/Header';
 import Register from './components/Pages/Register';
+import { useContext } from 'react';
+import UsersContext from './contexts/UsersContext';
 
 const App = () => {
+
+  const { logedUser } = useContext(UsersContext);
+
   return (
     <>
     <Header />
       <Routes>
-        <Route path='/askQuestion' element={<AskQuestion />} />
-        <Route path='/editQuestion/:id' element={<EditQuestion />}/>
+        <Route path='/askQuestion' element={logedUser ? <AskQuestion /> : <Navigate replace to={"/login"} />} />
+        <Route path='/editQuestion/:id' element={logedUser ? <EditQuestion /> : <Navigate replace to={"/login"} />}/>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<Home />}/>
